78a1e20ad82e8b45321bee05f2c8c2a2f8e25c6d
JP Dustin <97708492+JPDustin@users.noreply.github.com>
2024-09-16 19:55:00 -0400

    Compress photos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Document is ready!');
+document.addEventListener('DOMContentLoaded', function () {
 
     // Privacy Policy Modal
     const privacyPolicyLink = document.getElementById('privacy-policy-link');
     const privacyPolicyModal = document.getElementById('privacy-policy-modal');
     const closePrivacy = document.getElementById('close-privacy');
-    
-    privacyPolicyLink.addEventListener('click', function() {
+
+    privacyPolicyLink.addEventListener('click', function () {
         console.log('Privacy Policy link clicked');
         privacyPolicyModal.style.display = "block";
     });
 
-    closePrivacy.addEventListener('click', function() {
+    closePrivacy.addEventListener('click', function () {
         privacyPolicyModal.style.display = "none";
     });
 
@@ -19,18 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const termsOfServiceLink = document.getElementById('terms-of-service-link');
     const termsOfServiceModal = document.getElementById('terms-of-service-modal');
     const closeTerms = document.getElementById('close-terms');
-    
-    termsOfServiceLink.addEventListener('click', function() {
+
+    termsOfServiceLink.addEventListener('click', function () {
         console.log('Terms of Service link clicked');
         termsOfServiceModal.style.display = "block";
     });
 
-    closeTerms.addEventListener('click', function() {
+    closeTerms.addEventListener('click', function () {
         termsOfServiceModal.style.display = "none";
     });
 
     // Close modal when clicking outside of the modal content
-    window.addEventListener('click', function(event) {
+    window.addEventListener('click', function (event) {
         if (event.target === privacyPolicyModal) {
             privacyPolicyModal.style.display = "none";
         }
@@ -38,43 +37,4 @@ document.addEventListener('DOMContentLoaded', function() {
             termsOfServiceModal.style.display = "none";
         }
     });
-
-    // Handle form submission with AJAX
-    $('#contactForm').submit(function(event) {
-        event.preventDefault(); // Prevent the default form submission
-
-        // Clear previous messages
-        $('#formMessage').html('');
-
-        // Get form data
-        var formData = {
-            name: $('#name').val(),
-            email: $('#email').val(),
-            subject: $('#subject').val(),
-            message: $('#message').val()
-        };
-
-        // Perform AJAX request
-        $.ajax({
-            type: 'POST',
-            url: 'submit_contact_form.php',
-            data: formData,
-            dataType: 'json',
-            encode: true
-        }).done(function(response) {
-            if (response.success) {
-                // Clear the form
-                $('#contactForm')[0].reset();
-
-                // Display success message
-                $('#formMessage').html('<div class="success">Message has been sent successfully!</div>');
-            } else {
-                // Display error message
-                $('#formMessage').html('<div class="error">Error sending message. Please try again.</div>');
-            }
-        }).fail(function(data) {
-            // Display error message
-            $('#formMessage').html('<div class="error">Error sending message. Please try again.</div>');
-        });
-    });
-});
+})
